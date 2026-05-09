a01f7d2d67ca3b527277d96c546d69707672c67a
phamquochoang356@gmail.com <phamquochoang356@gmail.com>
2026-05-10 05:01:48 +0700

    tét

--- Rallyhub.Service/SepayService/Service.cs
+++ Rallyhub.Service/SepayService/Service.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Rallyhub.Repository;
 
@@ -20,255 +20,180 @@ public class Service : IService
 
     public async Task<bool> BookingSepayWebhookHandler(Request.SepayWebhookRequest request)
     {
-        // var description = request.Code;
-        // if (description.StartsWith("RA"))
-        // {
-        //     var raw = description.Replace("RA", "");
-        //
-        //     if (string.IsNullOrEmpty(raw) || raw.Length < 28)
-        //     {
-        //         throw new Exception("Error code");
-        //     }
-        //
-        //     var formatted =
-        //         $"{raw.Substring(0, 8)}-" +
-        //         $"{raw.Substring(8, 4)}-" +
-        //         $"{raw.Substring(12, 4)}-" +
-        //         $"{raw.Substring(16, 4)}-" +
-        //         $"{raw.Substring(20, 10)}";
-        //
-        //     Repository.Entity.Booking? targetBooking = null;
-        //
-        //     if (Guid.TryParse(formatted, out var exactGuid))
-        //     {
-        //         targetBooking = await _dbContext.Bookings
-        //             .Include(x => x.BookingDetails)
-        //             .FirstOrDefaultAsync(x => x.Id == exactGuid);
-        //     }
-        //
-        //     if (targetBooking == null)
-        //     {
-        //         targetBooking = await _dbContext.Bookings
-        //             .Include(x => x.BookingDetails)
-        //             .Where(x => EF.Functions.TrigramsSimilarity(x.Id.ToString(), formatted) > 0.68)
-        //             .OrderBy(x => EF.Functions.TrigramsSimilarityDistance(x.Id.ToString(), formatted))
-        //             .FirstOrDefaultAsync();
-        //     }
-        //
-        //     if (targetBooking == null)
-        //     {
-        //         throw new Exception("Not found");
-        //     }
-        //
-        //     if (targetBooking.Status != "Pending")
-        //     {
-        //         throw new Exception("Booking is completed");
-        //     }
-        //
-        //     if (targetBooking.FinalPrice != request.TransferAmount)
-        //     {
-        //         throw new Exception("Invalid transfer amount");
-        //     }
-        //
-        //     targetBooking.Status = "Banked";
-        //     targetBooking.UpdatedAt = DateTimeOffset.UtcNow;
-        //
-        //     var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == targetBooking.Customer.UserId);
-        //     if (user == null)
-        //     {
-        //         throw new Exception("User not found");
-        //     }
-        //
-        //     var wallet = await _dbContext.Wallets.FirstOrDefaultAsync(x => x.UserId == user.Id);
-        //     if (wallet == null)
-        //     {
-        //         throw new Exception("Wallet not found");
-        //     }
-        //
-        //     _dbContext.Update(targetBooking);
-        //     await _dbContext.SaveChangesAsync();
-        //
-        //     var transactionI = new Transaction.Request.CreateTransactionRequest()
-        //     {
-        //         Type = Transaction.Request.TypeList.Payment,
-        //         Amount = request.TransferAmount,
-        //         BalanceBefore = wallet.Balance,
-        //         BalanceAfter = wallet.Balance,
-        //         Status = "Success",
-        //         SePayId = request.Id.ToString(),
-        //         BankRefCode = request.ReferenceCode,
-        //         BankAccountNumber = request.AccountNumber,
-        //         TransferContent = request.Content,
-        //         ActionCode = request.Code,
-        //         Signature =  request.Description,
-        //         BookingId =  targetBooking.Id,
-        //         WalletId = wallet.Id,
-        //     };
-        //     if (!await _transactionService.CreateTransaction(transactionI))
-        //     {
-        //         throw new Exception("Error creating transaction");
-        //     }
-        //
-        //     return true;
-        // }
-        // else
-        // {
-        //     // (description.StartsWith("WA"))
-        //     var raw = description.Replace("WA", "");
-        //
-        //     if (string.IsNullOrEmpty(raw) || raw.Length < 28)
-        //     {
-        //         throw new Exception("Error code");
-        //     }
-        //
-        //     var formatted =
-        //         $"{raw.Substring(0, 8)}-" +
-        //         $"{raw.Substring(8, 4)}-" +
-        //         $"{raw.Substring(12, 4)}-" +
-        //         $"{raw.Substring(16, 4)}-" +
-        //         $"{raw.Substring(20, 10)}";
-        //
-        //     Repository.Entity.Wallet? targetWallet = null;
-        //     if (Guid.TryParse(formatted, out var exactGuid))
-        //     {
-        //         targetWallet = await _dbContext.Wallets
-        //             .FirstOrDefaultAsync(x => x.Id == exactGuid);
-        //     }
-        //
-        //     if (targetWallet == null)
-        //     {
-        //         targetWallet = await _dbContext.Wallets
-        //             .Where(x => EF.Functions.TrigramsSimilarity(x.Id.ToString(), formatted) > 0.68)
-        //             .OrderBy(x => EF.Functions.TrigramsSimilarityDistance(x.Id.ToString(), formatted))
-        //             .FirstOrDefaultAsync();
-        //     }
-        //
-        //     if (targetWallet == null)
-        //     {
-        //         throw new Exception("Not found");
-        //     }
-        //
-        //     var transaction =
-        //         await _dbContext.Transactions.FirstOrDefaultAsync(x =>
-        //             x.WalletId == targetWallet.Id && x.Status == "Pending");
-        //     if (transaction == null)
-        //     {
-        //         throw new Exception("Transaction not found");
-        //     }
-        //
-        //     if (transaction.Amount != request.TransferAmount)
-        //     {
-        //         throw new Exception("Invalid transfer amount");
-        //     }
-        //
-        //     if (!await _walletService.AddBanlanceToWallet(targetWallet.UserId, request.TransferAmount, "Wallet"))
-        //     {
-        //         throw new Exception("Wallet reject balance failed");
-        //     }
-        //
-        //     transaction.Status = "Success";
-        //     transaction.SePayId = request.Id.ToString();
-        //     transaction.BankRefCode = request.ReferenceCode;
-        //     transaction.BankAccountNumber = request.AccountNumber;
-        //     transaction.TransferContent = request.Content;
-        //     transaction.ActionCode = request.Code;;
-        //     transaction.Signature = request.Description;
-        //     transaction.UpdatedAt = DateTimeOffset.UtcNow;
-        //     _dbContext.Update(transaction);
-        //     var result = await _dbContext.SaveChangesAsync();
-        //     if (result > 0)
-        //     {
-        //         return true;
-        //     }
-        //
-        //     return false;
-        // }
         var description = request.Code;
-        var raw = description.Replace("RA", "");
-    
-        if (string.IsNullOrEmpty(raw) || raw.Length < 28)
+        if (string.IsNullOrEmpty(description))
         {
-            throw new Exception("Error code");
-        }
-        var formatted = $"{raw.Substring(0, 8)}-" +
-                        $"{raw.Substring(8, 4)}-" +
-                        $"{raw.Substring(12, 4)}-" +
-                        $"{raw.Substring(16, 4)}-" +
-                        $"{raw.Substring(20, 10)}";
-        
-        Repository.Entity.Booking? targetBooking = null;
-
-        if (Guid.TryParse(formatted, out var exactGuid))
-        {
-            targetBooking = await _dbContext.Bookings
-                .Include(x => x.BookingDetails)
-                .FirstOrDefaultAsync(x => x.Id == exactGuid);
+            throw new Exception("Description is empty");
         }
 
-        if (targetBooking == null)
+        if (description.StartsWith("RA"))
         {
-            targetBooking = await _dbContext.Bookings
-                .Include(x => x.BookingDetails)
-                .Where(x => EF.Functions.TrigramsSimilarity(x.Id.ToString(), formatted) > 0.68)
-                .OrderBy(x => EF.Functions.TrigramsSimilarityDistance(x.Id.ToString(), formatted))
-                .FirstOrDefaultAsync();
-        }
-        if (targetBooking == null)
-        {
-            throw new Exception("Not found");
-        }
-        if (targetBooking.Status != "Pending")
-        {
-            throw new Exception("Booking is completed");
-        }
-        if(targetBooking.FinalPrice != request.TransferAmount)
-        {
-            throw new Exception("Invalid transfer amount");
-        }
-        
-        
-        // var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == targetBooking.Customer.UserId);
-        // if (user == null)
-        // {
-        //     throw new Exception("User not found");
-        // }
-        //
-        // var wallet = await _dbContext.Wallets.FirstOrDefaultAsync(x => x.UserId == user.Id);
-        // if (wallet == null)
-        // {
-        //     throw new Exception("Wallet not found");
-        // }
-        
-        
-        targetBooking.Status = "Banked";
-        _dbContext.Update(targetBooking);
-        var result = await _dbContext.SaveChangesAsync();
-        
-        // var transactionI = new Transaction.Request.CreateTransactionRequest()
-        // {
-        //     Type = Transaction.Request.TypeList.Payment,
-        //     Amount = request.TransferAmount,
-        //     BalanceBefore = wallet.Balance,
-        //     BalanceAfter = wallet.Balance,
-        //     Status = "Success",
-        //     SePayId = request.Id.ToString(),
-        //     BankRefCode = request.ReferenceCode,
-        //     BankAccountNumber = request.AccountNumber,
-        //     TransferContent = request.Content,
-        //     ActionCode = request.Code,
-        //     Signature =  request.Description,
-        //     BookingId =  targetBooking.Id,
-        //     WalletId = wallet.Id,
-        // };
-        // if (!await _transactionService.CreateTransaction(transactionI))
-        // {
-        //     throw new Exception("Error creating transaction");
-        // }
-        if (result > 0)
-        {
+            var raw = description.Replace("RA", "");
+
+            if (string.IsNullOrEmpty(raw) || raw.Length < 28)
+            {
+                throw new Exception("Error code");
+            }
+
+            var formatted =
+                $"{raw.Substring(0, 8)}-" +
+                $"{raw.Substring(8, 4)}-" +
+                $"{raw.Substring(12, 4)}-" +
+                $"{raw.Substring(16, 4)}-" +
+                $"{raw.Substring(20, 10)}";
+
+            Repository.Entity.Booking? targetBooking = null;
+
+            if (Guid.TryParse(formatted, out var exactGuid))
+            {
+                targetBooking = await _dbContext.Bookings
+                    .Include(x => x.BookingDetails)
+                    .Include(x => x.Customer) // Sửa lỗi NullReferenceException tại đây
+                    .FirstOrDefaultAsync(x => x.Id == exactGuid);
+            }
+
+            if (targetBooking == null)
+            {
+                targetBooking = await _dbContext.Bookings
+                    .Include(x => x.BookingDetails)
+                    .Include(x => x.Customer) // Sửa lỗi NullReferenceException tại đây
+                    .Where(x => EF.Functions.TrigramsSimilarity(x.Id.ToString(), formatted) > 0.68)
+                    .OrderBy(x => EF.Functions.TrigramsSimilarityDistance(x.Id.ToString(), formatted))
+                    .FirstOrDefaultAsync();
+            }
+
+            if (targetBooking == null)
+            {
+                throw new Exception("Not found");
+            }
+
+            if (targetBooking.Status != "Pending")
+            {
+                throw new Exception("Booking is completed");
+            }
+
+            if (targetBooking.FinalPrice != request.TransferAmount)
+            {
+                throw new Exception("Invalid transfer amount");
+            }
+
+            targetBooking.Status = "Banked";
+            targetBooking.UpdatedAt = DateTimeOffset.UtcNow;
+
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == targetBooking.Customer.UserId);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            var wallet = await _dbContext.Wallets.FirstOrDefaultAsync(x => x.UserId == user.Id);
+            if (wallet == null)
+            {
+                throw new Exception("Wallet not found");
+            }
+
+            _dbContext.Update(targetBooking);
+            await _dbContext.SaveChangesAsync();
+
+            var transactionI = new Transaction.Request.CreateTransactionRequest()
+            {
+                Type = Transaction.Request.TypeList.Payment,
+                Amount = request.TransferAmount,
+                BalanceBefore = wallet.Balance,
+                BalanceAfter = wallet.Balance,
+                Status = "Success",
+                SePayId = request.Id.ToString(),
+                BankRefCode = request.ReferenceCode,
+                BankAccountNumber = request.AccountNumber,
+                TransferContent = request.Content,
+                ActionCode = request.Code,
+                Signature = request.Description,
+                BookingId = targetBooking.Id,
+                WalletId = wallet.Id,
+            };
+
+            if (!await _transactionService.CreateTransaction(transactionI))
+            {
+                throw new Exception("Error creating transaction");
+            }
+
             return true;
         }
-        return false;
+        else if (description.StartsWith("WA"))
+        {
+            var raw = description.Replace("WA", "");
 
+            if (string.IsNullOrEmpty(raw) || raw.Length < 28)
+            {
+                throw new Exception("Error code");
+            }
+
+            var formatted =
+                $"{raw.Substring(0, 8)}-" +
+                $"{raw.Substring(8, 4)}-" +
+                $"{raw.Substring(12, 4)}-" +
+                $"{raw.Substring(16, 4)}-" +
+                $"{raw.Substring(20, 10)}";
+
+            Repository.Entity.Wallet? targetWallet = null;
+            if (Guid.TryParse(formatted, out var exactGuid))
+            {
+                targetWallet = await _dbContext.Wallets
+                    .FirstOrDefaultAsync(x => x.Id == exactGuid);
+            }
+
+            if (targetWallet == null)
+            {
+                targetWallet = await _dbContext.Wallets
+                    .Where(x => EF.Functions.TrigramsSimilarity(x.Id.ToString(), formatted) > 0.68)
+                    .OrderBy(x => EF.Functions.TrigramsSimilarityDistance(x.Id.ToString(), formatted))
+                    .FirstOrDefaultAsync();
+            }
+
+            if (targetWallet == null)
+            {
+                throw new Exception("Not found");
+            }
+
+            var transaction =
+                await _dbContext.Transactions.FirstOrDefaultAsync(x =>
+                    x.WalletId == targetWallet.Id && x.Status == "Pending");
+            
+            if (transaction == null)
+            {
+                throw new Exception("Transaction not found");
+            }
+
+            if (transaction.Amount != request.TransferAmount)
+            {
+                throw new Exception("Invalid transfer amount");
+            }
+
+            if (!await _walletService.AddBanlanceToWallet(targetWallet.UserId, request.TransferAmount, "Wallet"))
+            {
+                throw new Exception("Wallet reject balance failed");
+            }
+
+            transaction.Status = "Success";
+            transaction.SePayId = request.Id.ToString();
+            transaction.BankRefCode = request.ReferenceCode;
+            transaction.BankAccountNumber = request.AccountNumber;
+            transaction.TransferContent = request.Content;
+            transaction.ActionCode = request.Code;
+            transaction.Signature = request.Description;
+            transaction.UpdatedAt = DateTimeOffset.UtcNow;
+            _dbContext.Update(transaction);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        else
+        {
+            throw new Exception("Unknown prefix");
+        }
     }
 }
